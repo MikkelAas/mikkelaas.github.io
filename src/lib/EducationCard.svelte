1611abf78ee5ff22d176a42678e74aae3f9394c0
Mikkel Aas <aa.mikkel2902@gmail.com>
2022-07-13 18:06:56 +0200

    formatted code

--- src/lib/EducationCard.svelte
+++ src/lib/EducationCard.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let place: string;
-    export let degree: string;
-    export let studyProgram: string;
+	export let degree: string;
+	export let studyProgram: string;
 	export let startDate: string;
 	export let endDate: string;
 	export let link: string;
@@ -9,15 +9,15 @@
 
 <div class="EducationCard">
 	<h3>{place}</h3>
-    <h4>{degree} in {studyProgram}</h4>
+	<h4>{degree} in {studyProgram}</h4>
 	<h5>Start {startDate} - End {endDate}</h5>
 	<a href={link}>View site</a>
 </div>
 
 <style>
-    .EducationCard h3, h4, h5 {
-    font-weight: 400;
-    }
-
-
+	.EducationCard h3,
+	h4,
+	h5 {
+		font-weight: 400;
+	}
 </style>
